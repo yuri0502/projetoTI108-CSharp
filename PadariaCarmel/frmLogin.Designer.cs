609d38aa9c99bb79d6e16bf369f146059ae27ce1
yuri0502 <yurisp0205@gmail.com>
2023-07-28 11:51:33 -0300

    projeto sistema padaria

---
+++ PadariaCarmel/frmLogin.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace PadariaCarmel
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.pctLogo = new System.Windows.Forms.PictureBox();
+            this.lblUsuário = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.txtUsuário = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.buttEntra = new System.Windows.Forms.Button();
+            this.buttSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctLogo
+            // 
+            this.pctLogo.Image = ((System.Drawing.Image)(resources.GetObject("pctLogo.Image")));
+            this.pctLogo.Location = new System.Drawing.Point(41, 123);
+            this.pctLogo.Name = "pctLogo";
+            this.pctLogo.Size = new System.Drawing.Size(316, 212);
+            this.pctLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctLogo.TabIndex = 0;
+            this.pctLogo.TabStop = false;
+            // 
+            // lblUsuário
+            // 
+            this.lblUsuário.AutoSize = true;
+            this.lblUsuário.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuário.Location = new System.Drawing.Point(421, 81);
+            this.lblUsuário.Name = "lblUsuário";
+            this.lblUsuário.Size = new System.Drawing.Size(64, 20);
+            this.lblUsuário.TabIndex = 1;
+            this.lblUsuário.Text = "Usuário";
+            // 
+            // lblSenha
+            // 
+            this.lblSenha.AutoSize = true;
+            this.lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSenha.Location = new System.Drawing.Point(421, 172);
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Size = new System.Drawing.Size(60, 20);
+            this.lblSenha.TabIndex = 2;
+            this.lblSenha.Text = "Senha:";
+            // 
+            // txtUsuário
+            // 
+            this.txtUsuário.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuário.Location = new System.Drawing.Point(421, 123);
+            this.txtUsuário.MaxLength = 50;
+            this.txtUsuário.Name = "txtUsuário";
+            this.txtUsuário.Size = new System.Drawing.Size(163, 26);
+            this.txtUsuário.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Wingdings", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.txtSenha.Location = new System.Drawing.Point(421, 216);
+            this.txtSenha.MaxLength = 20;
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = 'l';
+            this.txtSenha.Size = new System.Drawing.Size(163, 25);
+            this.txtSenha.TabIndex = 1;
+            this.txtSenha.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // buttEntra
+            // 
+            this.buttEntra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttEntra.Location = new System.Drawing.Point(421, 275);
+            this.buttEntra.Name = "buttEntra";
+            this.buttEntra.Size = new System.Drawing.Size(82, 41);
+            this.buttEntra.TabIndex = 2;
+            this.buttEntra.Text = "&Entra";
+            this.buttEntra.UseVisualStyleBackColor = true;
+            this.buttEntra.Click += new System.EventHandler(this.buttEntra_Click);
+            // 
+            // buttSair
+            // 
+            this.buttSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttSair.Location = new System.Drawing.Point(545, 275);
+            this.buttSair.Name = "buttSair";
+            this.buttSair.Size = new System.Drawing.Size(82, 41);
+            this.buttSair.TabIndex = 3;
+            this.buttSair.Text = "Sai&r";
+            this.buttSair.UseVisualStyleBackColor = true;
+            this.buttSair.Click += new System.EventHandler(this.buttSair_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 561);
+            this.Controls.Add(this.buttSair);
+            this.Controls.Add(this.buttEntra);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuário);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblUsuário);
+            this.Controls.Add(this.pctLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Padaria - Login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctLogo;
+        private System.Windows.Forms.Label lblUsuário;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.TextBox txtUsuário;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button buttEntra;
+        private System.Windows.Forms.Button buttSair;
+    }
+}
---
+++ PadariaCarmel/frmLogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PadariaCarmel
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttEntra_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bem vindo ao sistema",
+                "Mensagem do sistema",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button3);
+        }
+    }
+}
